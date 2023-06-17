26a768e14deabf5b4e9579d72dc789a38ef6a267
RageNUB <jirimon39@gmail.com>
2023-06-17 23:33:47 +0600

    third commit

--- index.js
+++ index.js
@@ -7,7 +7,15 @@ const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.use(cors());
+// app.use(cors());
+const corsConfig = {
+  origin: "*",
+  credentials: true,
+  methods: ["GET","POST","PUT","DELETE"]
+}
+app.use(cors(corsConfig))
+app.options("",cors(corsConfig))
+
 app.use(express.json());
 
 const verifyJWT = (req, res, next) => {
